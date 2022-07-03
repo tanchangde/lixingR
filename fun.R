d3f820724e37244bc390d6f2db52212f03cf5726
Changde Tan <tanchangde@gmail.com>
2022-07-03 10:10:22 +0800

    Get information about the industry the stock belongs to.

--- fun.R
+++ fun.R
@@ -132,6 +132,21 @@ get_cn_company_indices <- function(token = NULL, stock_codes = NULL, date = NULL
     dplyr::select(-c(code, message))
 }
 
+get_cn_company_industries <- function(token = NULL, stock_code = NULL, date = NULL) {
+  url <- "https://open.lixinger.com/api/cn/company/industries"
+  api_type <- url %>%
+    stringr::str_match(., "company.*$") %>%
+    stringr::str_replace_all(., "/", "_")
+  create_post(
+    url = url, api_type = api_type, token = token, stock_codes = stock_code,
+    date = date
+  ) %>%
+    httr::content(., as = "parsed", encoding = "utf-8") %>%
+    tibble::as_tibble(.) %>%
+    tidyr::unnest_wider(., col = data) %>%
+    dplyr::select(-c(code, message))
+}
+
 get_cn_company_fundamental <- function(financial_report_type = "non_financial",
                                token = NULL, date = NULL,
                                start_date = NULL, end_date = NULL,
